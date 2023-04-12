e02b65d63b6c2f3854b210e443cc51b6c78f8d77
Devender Pal <57867128+devender15@users.noreply.github.com>
2023-04-13 00:13:52 +0530

    data for corousel

---
+++ src/data/corousel-data.js
@@ -0,0 +1,14 @@
+export const DATA = [
+    {
+        id: 1,
+        img: '../src/assets/c1.png',
+        title: 'Conversations today could lead to opportunity tomorrow',
+        desc: 'Sending messages to people you know is a great way to strengthen relationships as you take the next step in your career.'
+    },
+    {
+        id: 2,
+        img: '../src/assets/c2.png',
+        title: 'Stay up to date on your industry',
+        desc: 'From live videos, to stories, to newsletters and more, LinkedIn is full of ways to stay up to date on the latest discussions in your industry.'
+    },
+]
